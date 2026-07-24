074ea5cda8a46a61fef8d38fc80fc0f933593dfa
vinodhini-netti <nettivinodhini@gmail.com>
2026-07-24 15:33:43 +0530

    Add files via upload

---
+++ ColumnSumArray.java
@@ -0,0 +1,13 @@
+public class ClumnSumArray
+{
+    public static void main(String args[]){
+        int[][] arr={{3,4,5},{5,6,7},{4,7,8}};
+        for(int j=0;j<arr[0].length;j++){
+            int sum=0;
+            for(int i=0;i<arr.length;i++){
+                sum+=arr[i][j];
+            }
+        System.out.println("column"+j+"="+sum);
+    }
+    }
+}
