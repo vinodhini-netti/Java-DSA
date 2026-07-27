5423a72c4a41a42134422af74ce4febbe37e3ab0
vinodhini-netti <nettivinodhini@gmail.com>
2026-07-27 14:49:53 +0530

    Add files via upload

---
+++ SetExplain.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class SetExplain {
+    public static void main(String args[]){
+        int arr[]={222,42,42,8};
+       HashSet<Integer>h=new HashSet<>();
+       for(int ele:arr){
+           h.add(ele);
+       }
+           System.out.println(h);
+           }
+       }
+
+
